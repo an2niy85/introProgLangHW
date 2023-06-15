7fe068e49fe78153b4ccfe0f4b6bbb6d7ab38adc
an2niy85 <an2niy85@mail.ru>
2023-06-15 07:58:54 +0400

    Add description for task num. 2

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.Clear();
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.Clear();
 Console.WriteLine("Введите два числа");
 Console.Write("Первое число a: ");
 int a = int.Parse(Console.ReadLine());
