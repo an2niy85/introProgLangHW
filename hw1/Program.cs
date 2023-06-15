d298a1b4defbdaee752b376d8da475fee745c6f6
an2niy85 <an2niy85@mail.ru>
2023-06-15 07:39:06 +0400

    Add hw1 console app

---
+++ hw1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два числа");
+Console.Write("Первое число a: ");
+int a = int.Parse(Console.ReadLine());
+//Console.WriteLine();
+Console.Write("Второе число b: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b) Console.WriteLine($"max = {a}");
+else Console.WriteLine($"max = {b}");
