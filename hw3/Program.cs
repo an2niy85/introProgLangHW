6304195a5d2de255fc51f84e27d5a4b92b4441cf
an2niy85 <an2niy85@mail.ru>
2023-06-15 16:50:25 +0400

    Add solution and description for hw3

--- hw3/Program.cs
+++ hw3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.Clear();
+Console.WriteLine("Введите число");
+Console.Write("Введеное число a: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0) Console.WriteLine("является четным");
+else Console.WriteLine("не является четным");
